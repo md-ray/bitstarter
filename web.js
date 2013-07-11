43260edac63543116faeaa63dbdc8b13259f61bf
Muhammad Rayhan <saviourcat@gmail.com>
2013-07-11 17:10:43 +0000

    Modify web.js

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
-
+var fs = require('fs');
+var data = '';
+fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  str = data;
+});
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
